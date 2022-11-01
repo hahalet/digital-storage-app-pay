6ed48e4d2e4ed25926a8b48456942d58de07894d
luolaigang <luolaigang@newstarfintech.com>
2022-11-01 17:04:35 +0800

    支付回调测试2

--- src/main/java/com/zhongqijia/pay/DigitalStoragePayApplication.java
+++ src/main/java/com/zhongqijia/pay/DigitalStoragePayApplication.java
@@ -2,6 +2,8 @@ package com.zhongqijia.pay;
 
 import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import com.alicp.jetcache.anno.config.EnableMethodCache;
+import com.zhongqijia.pay.utils.CertUtil;
+import com.zhongqijia.pay.utils.SDKConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +14,14 @@ public class DigitalStoragePayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalStoragePayApplication.class, args);
+		// 加载配置文件
+		SDKConfig.getConfig().loadPropertiesFromSrc();
+		// 加载证书
+		try {
+			CertUtil.init(SDKConfig.getConfig().getSandCertPath(), SDKConfig.getConfig().getSignCertPath(), SDKConfig.getConfig().getSignCertPwd());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/zhongqijia/pay/controller/SandController.java
+++ src/main/java/com/zhongqijia/pay/controller/SandController.java
@@ -1,13 +1,14 @@
 package com.zhongqijia.pay.controller;
 
-import cn.com.sand.ceas.sdk.config.CertCache;
-import cn.com.sand.ceas.sdk.config.ConfigLoader;
-import cn.com.sand.ceas.sdk.util.SignatureUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zhongqijia.pay.config.BusConfig;
 import com.zhongqijia.pay.event.AppEventSender;
+import com.zhongqijia.pay.utils.CertUtil;
+import com.zhongqijia.pay.utils.CryptoUtil;
+import com.zhongqijia.pay.utils.SDKConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SignatureException;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Map;
 
 /**
@@ -34,13 +36,6 @@ public class SandController {
 
     @Autowired
     private AppEventSender appEventSender;
-
-    /**
-     * 加载配置文件
-     */
-    public static void loadCertFromSrc() {
-        ConfigLoader.getConfig().loadPropertiesFromSrc();
-    }
 
     /**
      * 功能描述: 钱包回调地址
@@ -62,19 +57,8 @@ public class SandController {
         appEventSender.send(BusConfig.SAND_WALLET_CALLBACK_ROUTING_KEY, JSONObject.parseObject(response));
         return "SUCCESS";
     }
-
-    /**
-     * 验签方法
-     */
-    private static boolean verifySign(JSONObject dataJson) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        String sign = dataJson.getString("sign");
-        String signType = dataJson.getString("signType");
-        dataJson.remove("sign");
-        dataJson.remove("signType");
-        String plainText = dataJson.getString("data");
-        return SignatureUtils.verify(plainText, sign, signType, CertCache.getCertCache().getPublicKey());
-    }
-
+    // 默认配置的是UTF-8
+    public static String encoding = "UTF-8";
     /**
      * 功能描述: 支付回调地址
      *
@@ -83,7 +67,7 @@ public class SandController {
      * @auther: xy
      */
     @PostMapping(value = "/payCallback")
-    public String payCallback(HttpServletRequest req, HttpServletResponse resp) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+    public String payCallback(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         log.info("payCallback 开始");
         Map<String, String[]> parameterMap = req.getParameterMap();
         log.info("获取到sand response为{}", JSON.toJSONString(parameterMap));
@@ -91,43 +75,17 @@ public class SandController {
             String data = req.getParameter("data");
             String sign = req.getParameter("sign");
             String signType = req.getParameter("signType");
-            // 验证签名
-            boolean valid;
-
-            //try {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("data", data);
-            jsonObject.put("sign", sign);
-            jsonObject.put("signType", signType);
-            //执行verifySign方法
-            valid = verifySign(jsonObject);
-            if (!valid) {//如果验签失败
-                log.error("verify sign fail.");
-                log.error("验签失败的签名字符串(data)为：" + data);
-                log.error("验签失败的签名值(sign)为：" + sign);
-            } else {//验签成功
-                log.info("verify sign success");
-                JSONObject dataJson = JSONObject.parseObject(data);
-                if (dataJson != null) {
-                    log.info("接收到的异步通知数据为：\n" + JSONObject.toJSONString(dataJson, true));
-                } else {
-                    log.error("接收异步通知数据异常！！！");
-                }
-                return "respCode=000000";
+            log.info("data:{}", data);
+            log.info("sign:{}", sign);
+            boolean valid = CryptoUtil.verifyDigitalSign(data.getBytes(encoding), Base64.decodeBase64(sign),
+                    CertUtil.getPublicKey(), "SHA1WithRSA");
+            if (!valid) {
+                System.out.println("验签失败");
+            } else {
+                System.out.println("验签成功");
             }
-            /*} catch (Exception e){
-                log.error(e.getMessage());
-            }*/
+            return "SUCCESS";
         }
-        //log.info("获取到sand response为{}", resp);
-
-        // appEventSender.send(BusConfig.SAND_PAY_CALLBACK_ROUTING_KEY, JSONObject.parseObject(resp.));
-        /**
-         * do something
-         */
-        // 收到天河链回调通知需回写大写“SUCCESS”，如没有回写则最多通知 9次，
-        // 重试延迟时间分别为：5,5,20,270,600,900,1800,3600,14400（秒），9次后没有拿到回写则停止通知
-        //return "SUCCESS";
         return null;
     }
 }
