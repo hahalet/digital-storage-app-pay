7884f25f613e8b24a3374e44e0ca905034ed6b43
luolaigang <luolaigang@newstarfintech.com>
2022-11-03 11:18:16 +0800

    开户回调验签修改

--- src/main/java/com/zhongqijia/pay/controller/SandController.java
+++ src/main/java/com/zhongqijia/pay/controller/SandController.java
@@ -20,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -51,17 +54,61 @@ public class SandController {
      * @auther: xy
      */
     @PostMapping(value = "/walletCallback")
-    public String walletCallback(HttpServletRequest req, HttpServletResponse resp) {
+    public String walletCallback(HttpServletRequest req, HttpServletResponse resp) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException {
         Map<String, String[]> parameterMap = req.getParameterMap();
         log.info("获取到sand walletCallback response为{}", JSON.toJSONString(parameterMap));
+        if(parameterMap != null && !parameterMap.isEmpty()) {
+            String data=req.getParameter("data");
+            String sign=req.getParameter("sign");
+            String signType =req.getParameter("signType");
+            //log.info("data:{}", data);
+            //log.info("sign:{}", sign);
+            // 验证签名
+            boolean valid;
+            //try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("data",data);
+            jsonObject.put("sign",sign);
+            jsonObject.put("signType","SHA1WithRSA");
+            //执行verifySign方法
+            valid = CryptoUtil.verifySignC2C(jsonObject);
+            if (!valid) {//如果验签失败
+                log.error("verify sign fail.");
+                log.error("验签失败的签名字符串(data)为："+ data);
+                log.error("验签失败的签名值(sign)为："+ sign);
+            }else {//验签成功
+                log.info("verify sign success");
+                JSONObject dataJson = JSONObject.parseObject(data);
+                    /*if (dataJson != null) {
+                        log.info("接收到的异步通知数据为：\n"+ JSONObject.toJSONString(dataJson, true));
+                    } else {
+                        log.error("接收异步通知数据异常！！！");
+                    }*/
 
-        /**
-         * do something
-         */
-        // 收到天河链回调通知需回写大写“SUCCESS”，如没有回写则最多通知 9次，
-        // 重试延迟时间分别为：5,5,20,270,600,900,1800,3600,14400（秒），9次后没有拿到回写则停止通知
-        //appEventSender.send(BusConfig.SAND_WALLET_CALLBACK_ROUTING_KEY, JSONObject.parseObject(response));
-        return "SUCCESS";
+                try{
+                    /*C2CSandCallBack c2CSandCallBack = JSONObject.parseObject(data, C2CSandCallBack.class);
+                    LogSandPayC2cCallBack logSandPayC2cCallBack = JSONObject.parseObject(data, LogSandPayC2cCallBack.class);
+                    logSandPayC2cCallBack.setPayeeAccNo(c2CSandCallBack.getPayeeInfo().getPayeeAccNo());
+                    logSandPayC2cCallBack.setPayeeMemId(c2CSandCallBack.getPayeeInfo().getPayeeMemID());
+                    logSandPayC2cCallBack.setPayeeAccName(c2CSandCallBack.getPayeeInfo().getPayeeAccName());
+                    logSandPayC2cCallBack.setPayerAccNo(c2CSandCallBack.getPayerInfo().getPayerAccNo());
+                    logSandPayC2cCallBack.setPayerMemId(c2CSandCallBack.getPayerInfo().getPayerMemID());
+                    logSandPayC2cCallBack.setPayerAccName(c2CSandCallBack.getPayerInfo().getPayerAccName());
+
+                    logSandPayC2cCallBack.setResponse(saveOperationRecord(req));
+                    logSandPayC2cCallBack.setCreateTime(DateUtils.getCurrentTimeStamp());
+                    logSandPayC2cCallBackMapper.insert(logSandPayC2cCallBack);*/
+                }catch(Exception e){
+                    log.info("保存sand walletCallback response失败:{}", e.getMessage());
+                }
+                appEventSender.send(BusConfig.SAND_PAY_CALLBACK_C2C_ROUTING_KEY, dataJson);
+                return "respCode=000000";
+            }
+            /*} catch (Exception e){
+                log.error(e.getMessage());
+            }*/
+        }
+        return null;
     }
     // 默认配置的是UTF-8
     public static String encoding = "UTF-8";
