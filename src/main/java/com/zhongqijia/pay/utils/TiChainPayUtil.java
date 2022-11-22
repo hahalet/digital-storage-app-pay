16b54f29036a892f0be280cde9490209d1a09cdf
luolaigang <luolaigang@newstarfintech.com>
2022-11-22 14:05:07 +0800

    杉德二级支付回调暂时取消

--- src/main/java/com/zhongqijia/pay/utils/TiChainPayUtil.java
+++ src/main/java/com/zhongqijia/pay/utils/TiChainPayUtil.java
@@ -8,5 +8,7 @@ public class TiChainPayUtil {
     public static String FIRST_ORDER = "FIT";
     //二手藏品
     public static String SECOND_ORDER = "SCD";
+    //盲盒
+    public static String MH_ORDER = "MH";
     public static String SUCCESS = "SUCCESS";
 }
