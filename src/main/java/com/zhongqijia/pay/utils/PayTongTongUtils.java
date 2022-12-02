965fa870a490535275a5cfb6f64a3173d438c7e9
luolaigang <luolaigang@newstarfintech.com>
2022-12-02 17:06:46 +0800

    统统支付添加

--- src/main/java/com/zhongqijia/pay/utils/PayTongTongUtils.java
+++ src/main/java/com/zhongqijia/pay/utils/PayTongTongUtils.java
@@ -98,7 +98,7 @@ public class PayTongTongUtils {
                              MyOrder myOrder, Collection collection,
                              String return_url, String notify_url, String domain, String payerClientIp, RedisUtil redisUtil){
         String jsonString = null;
-        try{
+        //try{
             Map<String, String> resUserInfo = PayTongTongUtils.getWalletInfo(users,tongtongPayRoot);
             String user_id = null;
             if(resUserInfo!=null && resUserInfo.get("resp_code").equals("000000")){
@@ -148,7 +148,7 @@ public class PayTongTongUtils {
             if(mer_order_no.length()<=30){
                 Object indexObj = redisUtil.get(RedisHelp.TT_PAY_ORDER_KEY+mer_order_no);
                 Integer index = 0;
-                if(index == null){
+                if(indexObj == null){
                     index = 0;
                 }else{
                     index = (Integer)indexObj;
@@ -177,9 +177,9 @@ public class PayTongTongUtils {
             //{"resp_code":"000000","redirect_url":"https://pay.sumpay.cn/cashier-fed/pages/wap/index.html#/?key=dc207e68-b08f-4316-ae05-f105887af7d8&pre=0"}
             //JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(res));
             jsonString = JSON.toJSONString(res);
-        }catch (Exception e){
+        /*}catch (Exception e){
             log.info("getWalletInfo error:{}",e.getMessage());
-        }
+        }*/
         return jsonString;
     }
 
